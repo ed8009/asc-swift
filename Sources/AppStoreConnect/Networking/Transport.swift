d99cadebee14d29c1ed61343ab6d24d44380e078
en.mitasov <en.mitasov@tensor.ru>
2023-08-24 11:09:29 +0300

    log

--- Sources/AppStoreConnect/Networking/Transport.swift
+++ Sources/AppStoreConnect/Networking/Transport.swift
@@ -38,6 +38,17 @@ public protocol Transport {
     func upload(request: URLRequest, data: Data, decoder: JSONDecoder) async throws -> Response<Data>
 }
 
+extension Data {
+
+    var prettyPrintedJSONString: NSString? {
+        guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
+              let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
+              let prettyPrintedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) else { return nil }
+
+        return prettyPrintedString
+    }
+}
+
 extension URLSession: Transport {
     /// Send the request and receive a ``Response`` asynchronously.
     /// - Parameters:
